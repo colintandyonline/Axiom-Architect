ee58f7dceca1ad99b0b90f903bcb71a4521baf38
Colin Tandy <colintandyonline@gmail.com>
2026-05-17 23:38:25 +0100

    Add Google Analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { AxiomGlobalFooter } from "../components/AxiomGlobalFooter";
 import { AxiomGlobalHeader } from "../components/AxiomGlobalHeader";
 import "./globals.css";
+
+const googleAnalyticsId = "G-WPVQ3WB3FK";
 
 export const metadata: Metadata = {
   title: "Axiom Architect",
@@ -24,6 +27,18 @@ export default function RootLayout({
         <AxiomGlobalHeader />
         {children}
         <AxiomGlobalFooter />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${googleAnalyticsId}');
+          `}
+        </Script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
